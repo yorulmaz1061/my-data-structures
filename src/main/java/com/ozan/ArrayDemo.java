00cca57827d9aa61ffe61c3a4d0d004dabadf767
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2024-01-25 10:57:13 +0300

    ArrayDemo and Assigment1

---
+++ src/main/java/com/ozan/ArrayDemo.java
@@ -0,0 +1,10 @@
+package com.ozan;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] scoreArray = new int[10];
+        System.out.println(scoreArray);
+
+    }
+
+}
